1fd044464e8b6eab27d2959286116f6a85893dbc
Jason Katz <jason_katz@brown.edu>
2018-05-15 15:46:31 -0400

    Update CAOS.jl

--- src/CAOS.jl
+++ src/CAOS.jl
@@ -1,5 +1,18 @@
 module CAOS
 
-# package code goes here
+using Bio.Tools.BLAST
+using Bio.Seq
+using JSON
+using FastaIO
+using Tokenize
+
+export generate_caos_rules, classify_new_sequence, load_tree
+
+include("caos_functions.jl")
+include("tree_functions.jl")
+include("utils.jl")
+include("classification.jl")
+include("gap_imputation.jl")
+include("user_functions.jl")
 
 end # module
