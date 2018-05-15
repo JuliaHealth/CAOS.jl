76a9ebb827719f89e27df94d377acdb75468e17a
Jason Katz <jason_katz@brown.edu>
2018-05-15 16:00:06 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,13 @@
 using CAOS
-@static if VERSION < v"0.7.0-DEV.2005"
-    using Base.Test
-else
-    using Test
-end
 
-# write your own tests here
-@test 1 == 2
+tree, character_labels, taxa_labels = generate_caos_rules("/data/S10593.nex", "/data/output")
+
+@test typeof(tree) == Node
+
+classification = classify_new_sequence(tree, character_labels, taxa_labels, "/data/Example_Sequence.txt", "/data/output")
+
+@test typeof(classification) == String
+
+tree2, character_labels2, taxa_labels2 = load_tree("/data/output")
+
+@test typeof(tree2) == Node
