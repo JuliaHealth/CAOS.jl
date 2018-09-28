cf30b7199635b5d44bc866a33a3f45b3d4c1e1cc
fernandogelin <fernandogelin@gmail.com>
2018-09-28 10:23:04 -0400

    adds documentation set up

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+using Documenter, CAOS
+# using Literate
+
+# compile all examples in BioMedQuery/examples/literate_src into markdown and jupyter notebooks for documentation
+# for (root, dirs, files) in walkdir("examples/literate_src")
+#     for file in files
+#         Literate.notebook(joinpath(root,file), joinpath(@__DIR__, "src", "notebooks"))
+#     end
+# end
+#
+# for (root, dirs, files) in walkdir("examples/literate_src")
+#     for file in files
+#         Literate.markdown(joinpath(root,file), joinpath(@__DIR__, "src", "examples"))
+#     end
+# end
+
+makedocs()
+
+deploydocs(
+    deps   = Deps.pip("mkdocs", "mkdocs-material"),
+    repo = "github.com/bcbi/CAOS.jl.git",
+    julia  = "0.7",
+    osname = "linux"
+)
