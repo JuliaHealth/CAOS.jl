70b3fb8a4f633740c0b9918b1b5d2dbad6a39769
fernandogelin <fernandogelin@gmail.com>
2018-09-28 10:30:33 -0400

    adds version to mkdocs on travis

--- docs/make.jl
+++ docs/make.jl
@@ -17,7 +17,7 @@ using Documenter, CAOS
 makedocs()
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "mkdocs-material"),
+    deps   = Deps.pip("mkdocs==1", "mkdocs-material==3.0.3"),
     repo = "github.com/bcbi/CAOS.jl.git",
     julia  = "0.7",
     osname = "linux"
