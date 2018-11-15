52ed12a61d50f08f3bb8c83d9fbc7c159cc61f25
fernandogelin <fernandogelin@gmail.com>
2018-11-15 16:12:22 -0500

    adds test for classification of protein seq

--- test/classification.jl
+++ test/classification.jl
@@ -15,8 +15,7 @@
 
         classification2 = classify_new_sequence(tree2, character_labels2, taxa_labels2, "data/protein_seq.txt", "data/output2", protein=true)
 
-        println(classification2)
-
+        @test classification2 == "HPV200"
 
         # @test classification == "Tubificoides_amplivasatus_II_CE1247"
     end
